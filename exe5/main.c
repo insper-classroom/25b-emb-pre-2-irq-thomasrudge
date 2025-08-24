c45b1719e20b3b459dac9592c6c1506c43cfb372
thomasrudge <thomasfr@al.insper.edu.br>
2025-08-24 01:09:45 -0300

    ex5

--- exe5/main.c
+++ exe5/main.c
@@ -3,9 +3,53 @@
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 
+const int led = 4;
+const int botao = 28;
+volatile int check = 0;
+
+
+void btn_callback(uint gpio , uint32_t events){
+  if (events == 0x4){ // falling edge
+      check = 1;
+  }
+  else if(events == 0x8){
+    check=0;
+  }
+}
+
 int main() {
     stdio_init_all();
 
+    stdio_init_all();
+    gpio_init(led);
+    gpio_init(botao);
+
+    gpio_set_dir(led, GPIO_OUT);
+    gpio_set_dir(botao, GPIO_IN);
+
+    gpio_pull_up(botao);
+
+    gpio_set_irq_enabled_with_callback(
+        botao, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+
+
     while (true) {
+        if (check){
+            absolute_time_t inicio = get_absolute_time();
+            while (check){
+
+            }
+            absolute_time_t fim = get_absolute_time();
+            int64_t delta_us = absolute_time_diff_us(inicio, fim);
+            double delta_ms = delta_us / 1000.0;
+
+
+            if (delta_ms > 800){
+                printf("Aperto longo!\n");
+            }
+            else{
+                printf("Aperto curto!\n");
+            }
+        }   
     }
 }
