673140e39ba90e3ebfa48c50c8794d194d94386c
thomasrudge <thomasfr@al.insper.edu.br>
2025-08-24 00:41:10 -0300

    ex2

--- exe2/main.c
+++ exe2/main.c
@@ -2,9 +2,35 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int led = 4;
+const int botao = 28;
+volatile int check = 0;
+
+void btn_callback(uint gpio , uint32_t events){
+  if (events == 0x4){ // falling edge
+      check = 1;
+  }
+  else if(events == 0x8){
+    check=0;
+  }
+
+}
+
 int main() {
   stdio_init_all();
+  gpio_init(led);
+  gpio_init(botao);
+
+  gpio_set_dir(led, GPIO_OUT);
+  gpio_set_dir(botao, GPIO_IN);
+
+  gpio_pull_up(botao);
+
+  gpio_set_irq_enabled_with_callback(
+      botao, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+
 
   while (true) {
+    gpio_put(led, check);
   }
 }
