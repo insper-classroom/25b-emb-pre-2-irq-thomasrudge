40883ae3af79408544065c20acefbd99bd5ad010
thomasrudge <thomasfr@al.insper.edu.br>
2025-08-24 00:23:47 -0300

    ex0

--- exe0/main.c
+++ exe0/main.c
@@ -2,18 +2,18 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-#include "hardware/gpio.h"
-#include "pico/stdlib.h"
-#include <stdio.h>
+// #include "hardware/gpio.h"
+// #include "pico/stdlib.h"
+// #include <stdio.h>
 
 const int BTN_PIN_R = 28;
 
-int btn_flag;
+volatile int btn_flag;
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
 
-    printf("btn pressed \n");
+    printf("btn pressed 22222 \n");
 
     while (!gpio_get(BTN_PIN_R)) {
       sleep_ms(1);
@@ -35,8 +35,8 @@ int main() {
   gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
                                      &btn_callback);
 
-  volatile int capture_flag = 0;
-  int a;
+  int capture_flag = 0;
+  
   while (1) {
     if (btn_flag) {
       capture_flag = 1;
